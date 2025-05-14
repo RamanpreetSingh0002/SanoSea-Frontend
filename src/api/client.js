a336bbea5886d8a36ff98650e88b8f91f9747d6d
Ramanpreet Singh <singhramanpreet964@gmail.com>
2025-05-14 21:22:24 +0530

    Updated on 14/05

--- src/api/client.js
+++ src/api/client.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
+// https://sanosea-backend.onrender.com
 // http://localhost:8000/api ---> instead of this use live server link after deploying
 const client = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://sanosea-backend.onrender.com/api",
 });
 
 export default client;
